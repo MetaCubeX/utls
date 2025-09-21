1c96ed5ee22d565c087ca3a6a6ef3fe4597d2928
Daniel McCarney <daniel@binaryparadox.net>
2025-09-21 12:49:22 +0800

    crypto/tls: empty server_name conf. ext. from server
    When a TLS server uses the information from the server_name extension in
    a client hello, and the connection isn't resuming, it should return an
    empty server_name extension in its server hello (or encrypted extensions
    for TLS 1.3).
    
    For TLS <1.3 we we do this in doFullHandshake(), by setting the
    pre-existing serverHelloMsg.serverNameAck bool. We know that the
    connection isn't resuming based on the context where this function is
    called.
    
    For TLS 1.3, a new encryptedExtensionsMsg.serverNameAck bool is added,
    and populated as appropriate in sendServerParameters() based on whether
    the conn was resumed or not. The encryptedExtensionsMsg marshalling is
    updated to emit the encrypted extension based on that field.
    
    These changes allow enabling the ServerNameExtensionServer-* bogo tests
    that verify both the presence and absence of the server_name extension
    based on the relevant specifications.
    
    Resolves #74282
    Updates #72006
    
    Change-Id: I703bc2ec916b50906bdece7b7483a7faed7aa8e4
    Reviewed-on: https://go-review.googlesource.com/c/go/+/684795
    TryBot-Bypass: Daniel McCarney <daniel@binaryparadox.net>
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    Auto-Submit: Daniel McCarney <daniel@binaryparadox.net>

--- handshake_messages.go
+++ handshake_messages.go
@@ -7,8 +7,9 @@ package tls
 import (
 	"errors"
 	"fmt"
-	"golang.org/x/exp/slices"
 	"strings"
+
+	"golang.org/x/exp/slices"
 
 	"golang.org/x/crypto/cryptobyte"
 )
@@ -1045,6 +1046,7 @@ type encryptedExtensionsMsg struct {
 	quicTransportParameters []byte
 	earlyData               bool
 	echRetryConfigs         []byte
+	serverNameAck           bool
 
 	utls utlsEncryptedExtensionsMsgExtraFields // [uTLS]
 }
@@ -1081,6 +1083,10 @@ func (m *encryptedExtensionsMsg) marshal() ([]byte, error) {
 				b.AddUint16LengthPrefixed(func(b *cryptobyte.Builder) {
 					b.AddBytes(m.echRetryConfigs)
 				})
+			}
+			if m.serverNameAck {
+				b.AddUint16(extensionServerName)
+				b.AddUint16(0) // empty extension_data
 			}
 		})
 	})
@@ -1137,6 +1143,11 @@ func (m *encryptedExtensionsMsg) unmarshal(data []byte) bool {
 			if !extData.CopyBytes(m.echRetryConfigs) {
 				return false
 			}
+		case extensionServerName:
+			if len(extData) != 0 {
+				return false
+			}
+			m.serverNameAck = true
 		default:
 			// [UTLS SECTION START]
 			if !m.utlsUnmarshal(extension, extData) {
